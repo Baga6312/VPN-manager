5750ce81bec8da4cb723a1d562c3223c901168bd
Baga6312 <magroubertn@gmail.com>
2024-03-29 20:28:08 +0100

    linuxscript

--- site/server/scripts/windowsInstall.js
+++ site/server/scripts/windowsInstall.js
@@ -11,68 +11,48 @@ const Install_chocolaty = () => {
       ? console.log(`Error: ${err}`)
       : stdout == "AllSigned"
       ? null
-      : exec(
-          //  setting the execution policys to set AllSigne as a admin user
-          //  user will be prompted with UAC
-          `powershell -WindowStyle Hidden -command "start-process cmd -verb runas -argumentlist '/c powershell -WindowStyle Hidden -NoProfile -ExecutionPolicy Bypass -Command "Set-ExecutionPolicy RemoteSigned -Force -Scope CurrentUser"'"`,
-          { shell: "powershell.exe" },
-          (error, sttdout) => {
-            error ? console.error(`Error: ${error.message}`) : null;
-            exec(
-              `Get-${checkPolicies}`,
-              { shell: "powershell.exe" },
-              (err, stttdout, stderr) => {
-                err
-                  ? console.log(`Error :${err}`)
-                  : stderr
-                  ? console.log(`Standard Error: ${stderr}`)
-                  : console.log(`Execution Policy set to ${stttdout}`);
-              }
-            );
-          }
-        );
-  });
-};
+      : (
+        const url = "https://community.chocolatey.org/install.ps1";
+        https.get(url, (res) => {
+        const path = `${__dirname}/install.ps1`;
+        const file_path = fs.createWriteStream(path);
+        res.pipe(file_path);
+        file_path.on("finish", () => {
+          file_path.close();
+          console.log("download Complete");
+      });
+    });
+    // installing the script with admin privi as a process
+    
+        const lsProcess = spawn("powershell", [
+          "-command",
+          `start-process cmd -verb runas -WindowStyle Hidden -argumentlist '/c powershell C:\\Users\\asma123\\Desktop\\check\\install.ps1' `,
+        ]);
+  
+        lsProcess.stdout.on("data", (data) => {
+          console.log(`${data}`);
+        });
+  
+        lsProcess.stderr.on("data", (data) => {
+          console.log(`stderr:\n${data}`); // Use "stderr" for error output
+        });
+  
+        lsProcess.on("exit", (code) => {
+          console.log(`Process ended with ${code}`);
+        });
+      )
+    });
+  };
+
 const Check_Chocolaty = () => {
   // checking if chocolaty exists
-  exec("choco -v", (error, stdout) => {
+    exec("choco -v", (error, stdout) => {
     error 
-      ? ( false ) 
+      ? (false) 
       : (true) ; 
-  });
-};
-const install_script = () => {
-  // downlaod the script for chocolaty
-  const url = "https://community.chocolatey.org/install.ps1";
-  https.get(url, (res) => {
-    const path = `${__dirname}/install.ps1`;
-    const file_path = fs.createWriteStream(path);
-    res.pipe(file_path);
-    file_path.on("finish", () => {
-      file_path.close();
-      console.log("download Complete");
     });
-  });
-
-  // installing the script with admin privi as a process
-  const lsProcess = spawn("powershell", [
-    // "-WindowStyle Hidden",
-    "-command",
-    `start-process cmd -verb runas -WindowStyle Hidden -argumentlist '/c powershell C:\\Users\\asma123\\Desktop\\check\\install.ps1' `,
-  ]);
-
-  lsProcess.stdout.on("data", (data) => {
-    console.log(`${data}`);
-  });
-
-  lsProcess.stderr.on("data", (data) => {
-    console.log(`stderr:\n${data}`); // Use "stderr" for error output
-  });
-
-  lsProcess.on("exit", (code) => {
-    console.log(`Process ended with ${code}`);
-  });
-};
+    Install_chocolaty()
+  };
 
 
 const Check_Install_Git = () => { 
@@ -93,8 +73,23 @@ const Check_Install_Make = () => {
 
 
 // clone the wireguard repo here 
-const Clone_repo = () => { 
+const Install_wireguard = () => { 
 exec('git clone https://github.com/WireGuard/wireguard-tools ' , {shell : "powershell.exe"} , (stderr , err , stdout)=> { if (stdout) => console.log(stdout)})
 exec('cd wireguard-tools && cd src ') 
 exec('make ')
 }
+
+const Install_dep = () => {
+  Check_Install_Git()
+  Check_Install_Make()
+  Install_wireguard()
+}
+// the main fking script bitch 
+
+module.exports.main = () => { 
+  // first check if dependecis exists 
+  Check_Chocolaty()
+  ? Install_dep()
+  : (Check_Chocolaty() ; Install_dep();)
+}
+
