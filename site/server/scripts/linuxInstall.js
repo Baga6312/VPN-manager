9f5fbd8423bcbf538b652b054b4d1e1df2e5175f
Baga6312 <magroubertn@gmail.com>
2024-03-30 07:21:20 +0100

    windowsInstall

--- site/server/scripts/linuxInstall.js
+++ site/server/scripts/linuxInstall.js
@@ -1,53 +1,46 @@
-const os = require("os")
-const fs = require("fs") 
 const {exec} = require("child_process");
 
  
 const Download_the_linux_shit = () => {
 
     if (getDist() == "Arch Linux") { 
-       exec("sudo pacman -S linux-headers base-devel pkg-config" , (error, stdout, stderr) => {
+          exec("sudo pacman -S linux-headers base-devel pkg-config" , (error, stdout, stderr) => {
 
-       console.log(`stdout: ${stdout}`)})
+          console.log(`stdout: ${stdout}`)})
 
-       exec("sudo pacman -S --noconfirm  wireguard", (error, stdout, stderr) => {
+          exec("sudo pacman -S --noconfirm  wireguard", (error, stdout, stderr) => {
 
-       if (stderr) {
+          if (stderr) {
 
-          console.log(`stderr: ${stderr}`)
-          return;
+             console.log(`stderr: ${stderr}`)
+             return;
 
-       } else { 
+          } else { 
 
-          console.log(`stdout: ${stdout}`)
+             console.log(`stdout: ${stdout}`)
 
-       })
-        
+          })
+          
     } else if (getDist() == "Ubuntu") {
 
-        exec("sudo apt-get install libelf-dev linux-headers-$(uname -r) build-essential pkg-config" , (error, stdout, stderr) => {
+          exec("sudo apt-get install libelf-dev linux-headers-$(uname -r) build-essential pkg-config" , (error, stdout, stderr) => {
 
-        console.log(`stdout: ${stdout}`)})
+          console.log(`stdout: ${stdout}`)})
 
-        exec("sudo apt install -y wireguard", (error, stdout, stderr) => {
+          exec("sudo apt install -y wireguard", (error, stdout, stderr) => {
 
-        if (stderr) {
-          console.log(`stderr: ${stderr}`)
-          return;
-        } else {
-          console.log(`stdout: ${stdout}`)
-        }
+          if (stderr) {
+            console.log(`stderr: ${stderr}`)
+            return;
+          } else {
+            console.log(`stdout: ${stdout}`)
+          }
     })
 }
 
   // the only way is to compile from source and use the executable 
   // TODO : scripts 
-  // download the github repo 
-  // building 
-  // find a way to use make without downloading it 
-  //   functions to be made  : 
-  //        checkChocolaty()
-  //        checkMake()
-  //        checkGit()
-
+  // checkDistro()
+  // checkWireguard()
+  // Compile_from_source() 
 
--- site/server/scripts/windowsInstall.js
+++ site/server/scripts/windowsInstall.js
@@ -1,95 +1,89 @@
-const { exec, spawn } = require("child_process");
-const https = require("https");
-const fs = require("fs");
+import psList from "ps-list";
 
-const Install_chocolaty = () => {
-  // automating the Execution policy for the user
-  // getting the state of the Execution policy
-  const checkPolicies = "ExecutionPolicy";
-  exec(`Get-${checkPolicies}`, { shell: "powershell.exe" }, (err, stdout) => {
-    err
-      ? console.log(`Error: ${err}`)
-      : stdout == "AllSigned"
-      ? null
-      : (
-        const url = "https://community.chocolatey.org/install.ps1";
-        https.get(url, (res) => {
-        const path = `${__dirname}/install.ps1`;
-        const file_path = fs.createWriteStream(path);
-        res.pipe(file_path);
-        file_path.on("finish", () => {
-          file_path.close();
-          console.log("download Complete");
-      });
+import { exec, spawn } from "child_process";
+import https from "https";
+import fs from "fs";
+import os from "os";
+
+// make a fking script to run as a admin contains all the dependecis check
+// Exec_script()
+
+const execScript = async () => {
+  //     start-process powershell -verb runas -argumentlist '/c powershell -ExecutionPolicy Bypass -File C:\\Users\\asma123\\Desktop\\check\\install.ps1',
+  const scriptPath = "C:\\Users\\asma123\\Desktop\\check\\install.ps1";
+  exec(
+    `Start-Process powershell -ArgumentList "-ExecutionPolicy Bypass", "-File", "${scriptPath}" -Verb RunAs`,
+    { shell: "powershell.exe" },
+    (error, stdout, stderr) => {
+      if (error) {
+        console.error(exec error: ${error});
+        return;
+      }
+      console.log(stdout: ${stdout});
+      console.error(stderr: ${stderr});
+    }
+  );
+};
+
+const clone_repo = (directoryPath) => {
+  const wireguardProcess = spawn("powershell", [
+    "git",
+    "clone",
+    "https://github.com/WireGuard/wireguard-tools",
+    ${directoryPath},
+  ]);
+
+  wireguardProcess.stdout.on("data", (data) => {
+    console.log(${data});
+  });
+
+  wireguardProcess.stderr.on("data", (data) => {
+    console.log(stderr:\n${data}); // Use "stderr" for error output
+  });
+
+  wireguardProcess.on("exit", (code) => {
+    console.log(Process ended with ${code});
+  });
+};
+
+const Exec_script = async () => {
+  // creating directory on appdata
+  const appdataPath = ${os.homedir()}\\AppData\\Roaming;
+  const newDirName = "urmom";
+  const directoryPath = ${appdataPath}\\${newDirName};
+  const url = "https://community.chocolatey.org/install.ps1";
+
+  try {
+    if (!fs.existsSync(directoryPath)) {
+      fs.mkdirSync(directoryPath);
+      console.log(Directory created successfully: ${directoryPath});
+    } else {
+      console.log(Directory already exists: ${directoryPath});
+    }
+  } catch (err) {
+    console.error("Error creating directory:", err);
+  }
+
+  // downloading the file on appdata
+  let path;
+  https.get(url, (res) => {
+    path = ${directoryPath}\\install.ps1;
+    const file_path = fs.createWriteStream(path);
+
+    res.pipe(file_path);
+    file_path.on("finish", () => {
+      file_path.close();
+      console.log("download Complete");
     });
-    // installing the script with admin privi as a process
-    
-        const lsProcess = spawn("powershell", [
-          "-command",
-          `start-process cmd -verb runas -WindowStyle Hidden -argumentlist '/c powershell C:\\Users\\asma123\\Desktop\\check\\install.ps1' `,
-        ]);
-  
-        lsProcess.stdout.on("data", (data) => {
-          console.log(`${data}`);
-        });
-  
-        lsProcess.stderr.on("data", (data) => {
-          console.log(`stderr:\n${data}`); // Use "stderr" for error output
-        });
-  
-        lsProcess.on("exit", (code) => {
-          console.log(`Process ended with ${code}`);
-        });
-      )
-    });
-  };
+  });
 
-const Check_Chocolaty = () => {
-  // checking if chocolaty exists
-    exec("choco -v", (error, stdout) => {
-    error 
-      ? (false) 
-      : (true) ; 
-    });
-    Install_chocolaty()
-  };
+  try {
+    execScript(directoryPath);
+  } catch (err) {
+    console.log(err);
+  }
+};
 
-
-const Check_Install_Git = () => { 
-  exec("git --version" , (stderr , err , stdout ) => { 
-    (stderr)
-    ?exec( `powershell -command "start-process cmd -verb runas -WindowStyle Hidden -argumentlist '/c choco install git -y"' `)
-    : null ; 
-  })
-}
-
-const Check_Install_Make = () => { 
-  exec("make -v" , (stderr , stdout)=> { 
-    (stderr)
-    ?exec( `powershell -command "start-process cmd -verb runas -WindowStyle Hidden -argumentlist '/c choco install make -y"' `)
-    :null ; 
-    console.log(stdout)
-  })
-
-
-// clone the wireguard repo here 
-const Install_wireguard = () => { 
-exec('git clone https://github.com/WireGuard/wireguard-tools ' , {shell : "powershell.exe"} , (stderr , err , stdout)=> { if (stdout) => console.log(stdout)})
-exec('cd wireguard-tools && cd src ') 
-exec('make ')
-}
-
-const Install_dep = () => {
-  Check_Install_Git()
-  Check_Install_Make()
-  Install_wireguard()
-}
-// the main fking script bitch 
-
-module.exports.main = () => { 
-  // first check if dependecis exists 
-  Check_Chocolaty()
-  ? Install_dep()
-  : (Check_Chocolaty() ; Install_dep();)
-}
-
+Exec_script().then(() => {
+  clone_repo();
+});
