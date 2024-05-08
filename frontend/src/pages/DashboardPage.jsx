8dbde830ea378507704d6647e3a4745be57c785a
Baga6312 <magroubertn@gmail.com>
2024-05-08 14:27:36 +0100

    fixed bugs

--- frontend/src/pages/DashboardPage.jsx
+++ frontend/src/pages/DashboardPage.jsx
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from 'react';
-import { Container} from 'react-bootstrap';
-import  authService from '../services/authService';
-import { useNavigate} from 'react-router-dom';
+import authService from '../services/authService';
 import '../assets/dashboard.css'
-import io from "socket.io-client"
-// const socket = io.connect("http://localhost:5000")
+import { Container} from 'react-bootstrap';
+import { useNavigate} from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMinus ,  faAngleUp , faAngleDown } from '@fortawesome/free-solid-svg-icons'
 
 const DashboardPage =() => {
   const [userData, setUserData] = useState([]);
@@ -25,17 +25,12 @@ const DashboardPage =() => {
 
         ws.onopen = () => {
         console.log('Connected to WebSocket server');
-      };
-
         ws.onmessage = (event) => {
         const data = JSON.parse(event.data);
         setInfo(data);
       };
 
-      
-      //  socket.on("data_cast" , (data)=> { 
-      //     setInfo(data)
-      //  })
+      };
      }, [history ]
   );
 
@@ -52,9 +47,31 @@ const DashboardPage =() => {
     <Container className="mt-4">
       <h2 id="header">Welcome, {userData[0]} </h2>
             <ul id="status">
-              <li><h4>VPN Connection Status </h4></li>
-              <li>{info && <p>{info} </p>}</li>
-              <li><p>{info=="" ? <span>\n \n</span> : null }</p></li>
+              <li>
+                <h4>VPN Connection Status</h4>
+              </li>
+              <li>
+                  {info !== "" ? <span class="whiet-spaces">
+                  <FontAwesomeIcon icon={faAngleUp}/> 
+                    <li>h</li> 
+                    { info.split(" ")[1] }
+                    { info.split(" ")[2]}
+                    <li>h</li> 
+                  <FontAwesomeIcon icon={faAngleDown}/>
+                    <li>h</li> 
+                    { info.split(" ")[4] }
+                    { info.split(" ")[5]}</span> 
+                    
+                    : <span class="whiet-spaces"> 
+                  <FontAwesomeIcon icon={faAngleUp} />
+                      <li>h</li> 
+                      <FontAwesomeIcon icon={faMinus}/>
+                      <FontAwesomeIcon icon={faMinus}/>
+                      <li>h</li> 
+                  <FontAwesomeIcon icon={faAngleDown}/>
+                      </span>
+                    }  
+                </li>
               <li><p className={info =="" ? 'active-circle' : 'inactive-circle' }>sex</p></li>
             </ul>
           <div id="container">
